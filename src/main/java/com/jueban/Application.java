5c359432a11938cd59d9f8c2a71dad529c16f138
jueban <q838687758@hotmail.com>
2017-11-25 12:14:02 +0800

    add spring boot config

---
+++ src/main/java/com/jueban/Application.java
@@ -0,0 +1,15 @@
+package com.jueban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
